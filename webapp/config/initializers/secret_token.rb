b1f1b6f948f7e5654852e8b667a04e6ab4b6b477
Carlos Gonzalez <carltronik@gmail.com>
2013-04-05 16:05:14 -0500

    Fifth Commit
    Added the ruby app in the webapp folder, cucumber/capybara/haml are
    already in the gemfile so you just have to execute bundle install

---
+++ webapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webapp::Application.config.secret_token = '36503ca68691d6cdf167a7462c253d6e9af5b5cd8bd8f7d370386ad955063662ff10e62bee8474c73c8237185d4d16f755e751b70e74859c819354a98f5ccd62'
