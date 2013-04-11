1ba34e1ce0bb1c09a9189fd7eda8f2b20c76fc03
mlcarter815 <michael-carter@uiowa.edu>
2013-04-11 13:21:25 -0700

    Added updates migration and updates controller

---
+++ webapp/db/migrate/20130411172641_create_users.rb
@@ -0,0 +1,9 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :Users do |t|
+      t.column :name, 'UserType', :null => false
+
+      t.timestamps
+    end
+  end
+end
