657d07cc8fe19e19b875829e72ddb561a931028c
mlcarter815 <michael-carter@uiowa.edu>
2013-04-11 15:24:46 -0700

    Modified Gem Cucumber and added drops to migrations

--- webapp/db/migrate/20130411172641_create_users.rb
+++ webapp/db/migrate/20130411172641_create_users.rb
@@ -1,9 +1,12 @@
 class CreateUsers < ActiveRecord::Migration
   def change
-    create_table :Users do |t|
+    create_table :users do |t|
       t.column :name, 'UserType', :null => false
 
       t.timestamps
     end
+  end
+  def down
+    drop_table :users
   end
 end
--- webapp/db/migrate/20130411200342_create_updates.rb
+++ webapp/db/migrate/20130411200342_create_updates.rb
@@ -1,6 +1,11 @@
 class CreateUpdates < ActiveRecord::Migration
   def change
     create_table :updates do |t|
+      t.datetime :date
+      t.text :text
     end
+  end
+  def down
+    drop_table :updates
   end
 end
