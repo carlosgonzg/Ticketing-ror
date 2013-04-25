0ba9102d61417831376138bc57f25e1d108f5675
meenal5254 <meenal-khandelwal@uiowa.edu>
2013-04-25 17:34:39 -0500

    login page

---
+++ webapp/app/controllers/login_page_controller.rb
@@ -0,0 +1,22 @@
+
+class LoginController < ApplicationController
+
+  def new
+    
+  end
+
+  def create
+    if user = User.authenticate(params[:Username],params[:password])
+      session[:user_id] = user.id
+      redirect_to users_url, :notice => 'login succeed'
+    else
+      redirect_to login_url, :alert => 'login failed'
+    end
+  end
+
+  def delete
+    session[:user_id] = nil
+    redirect_to login_url, :alert => 'loged out'
+  end
+
+end
